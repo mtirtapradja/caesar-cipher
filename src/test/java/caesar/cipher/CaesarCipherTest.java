1c9d053e02292abe6c78def53ecb3e8dcf621194
mtirtapradja <mtirtapradja@gmail.com>
2020-05-03 00:08:19 +0700

    Added new test

--- src/test/java/caesar/cipher/CaesarCipherTest.java
+++ src/test/java/caesar/cipher/CaesarCipherTest.java
@@ -5,23 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CaesarCipherTest {
-	
-	private CaesarCipher caesarCipher = new CaesarCipher(); 
+
+	private CaesarCipher caesarCipher = new CaesarCipher();
 
 	@Test
 	void testCipheredMessageWithOffsetOf12() {
-		assertEquals("tai mdq kag pauzs fapmk" , caesarCipher.cipher("how are you doing today", 12));
+		assertEquals("tai mdq kag pauzs fapmk", caesarCipher.cipher("how are you doing today", 12));
 	}
-	
+
 	@Test
 	void testEmptyString() {
-		assertEquals("" , caesarCipher.cipher("", 12));
+		assertEquals("", caesarCipher.cipher("", 12));
 	}
-	
-	/*
-	 * @Test void testCipheredMessageWithOffsetOf11() {
-	 * assertEquals("tai mdq kag pauzs fapmk" ,
-	 * caesarCipher.cipher("how are you doing today", 11)); }
-	 */
 
+	
+	@Test void testCipheredMessageWithOffsetOf11() {
+		assertEquals("tai mdq kag pauzs fapmk" ,
+		caesarCipher.cipher("how are you doing today", 11)); 
+	}
+	 
 }
